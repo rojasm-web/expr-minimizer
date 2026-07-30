5af93266a8aad52353ae47864d211db68595a3ed
rojasm-web <rojasm@awty.org>
2026-07-30 15:15:30 -0500

    Delete target directory

--- target/debug/build/crossbeam-deque-7448755c7ef261f7/build_script_build-7448755c7ef261f7.d
+++
@@ -1,5 +0,0 @@
-/Users/rojasm/Downloads/expr-minimizer/target/debug/build/crossbeam-deque-7448755c7ef261f7/build_script_build-7448755c7ef261f7.d: /Users/rojasm/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/crossbeam-deque-0.8.7/build.rs
-
-/Users/rojasm/Downloads/expr-minimizer/target/debug/build/crossbeam-deque-7448755c7ef261f7/build_script_build-7448755c7ef261f7: /Users/rojasm/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/crossbeam-deque-0.8.7/build.rs
-
-/Users/rojasm/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/crossbeam-deque-0.8.7/build.rs:
