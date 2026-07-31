ee37236b6acadcdb9cb80e7f39157c95cd75d072
rojasm-web <rojasm@awty.org>
2026-07-31 08:23:41 +0000

    weee

--- src/catalog.rs
+++ src/catalog.rs
@@ -57,7 +57,14 @@ impl SymbolicExpr {
             }
             SymbolicExpr::Add(a, b) => format!("{} + {}", a.format_clean(), b.format_clean()),
             SymbolicExpr::Sub(a, b) => format!("{} - {}", a.format_clean(), b.format_parenthesized()),
-            SymbolicExpr::Mul(a, b) => format!("{} * {}", a.format_parenthesized(), b.format_parenthesized()),
+
+            SymbolicExpr::Mul(a, b) => match (a.as_ref(), b.as_ref()) {
+                (SymbolicExpr::Num(1), SymbolicExpr::I) | (SymbolicExpr::I, SymbolicExpr::Num(1)) => "i".to_string(),
+                (SymbolicExpr::Num(-1), SymbolicExpr::I) | (SymbolicExpr::I, SymbolicExpr::Num(-1)) => "-i".to_string(),
+                (SymbolicExpr::Num(n), SymbolicExpr::I) | (SymbolicExpr::I, SymbolicExpr::Num(n)) => format!("{}i", n),
+                _ => format!("{} * {}", a.format_parenthesized(), b.format_parenthesized()),
+            },
+
             SymbolicExpr::Div(a, b) => format!("{}/{}", a.format_parenthesized(), b.format_parenthesized()),
             SymbolicExpr::Pow(a, b) => format!("{}^{}", a.format_parenthesized(), b.format_parenthesized()),
             SymbolicExpr::Ln(a) => format!("ln({})", a.format_clean()),
@@ -161,6 +168,13 @@ fn simplify_pass(expr: SymbolicExpr) -> SymbolicExpr {
                 SymbolicExpr::Pow(b, x) if *b == SymbolicExpr::E => *x,
                 SymbolicExpr::Num(0) => SymbolicExpr::Inf(false),
                 SymbolicExpr::Inf(true) => SymbolicExpr::Inf(true),
+                SymbolicExpr::I => SymbolicExpr::Div(Box::new(SymbolicExpr::Mul(Box::new(SymbolicExpr::I), Box::new(SymbolicExpr::Pi))), Box::new(SymbolicExpr::Num(2))),
+                SymbolicExpr::Neg(inner) if matches!(inner.as_ref(), SymbolicExpr::I) => SymbolicExpr::Neg(Box::new(SymbolicExpr::Div(Box::new(SymbolicExpr::Mul(Box::new(SymbolicExpr::I), Box::new(SymbolicExpr::Pi))), Box::new(SymbolicExpr::Num(2))))),
+                SymbolicExpr::Mul(a, b) if matches!(a.as_ref(), SymbolicExpr::Exp(_)) || matches!(b.as_ref(), SymbolicExpr::Exp(_)) => {
+                    let ln_a = simplify_pass(SymbolicExpr::Ln(a));
+                    let ln_b = simplify_pass(SymbolicExpr::Ln(b));
+                    simplify_pass(SymbolicExpr::Add(Box::new(ln_a), Box::new(ln_b)))
+                }
                 other => SymbolicExpr::Ln(Box::new(other)),
             }
         }
@@ -173,6 +187,9 @@ fn simplify_pass(expr: SymbolicExpr) -> SymbolicExpr {
                 SymbolicExpr::Ln(x) => *x,
                 SymbolicExpr::Inf(true) => SymbolicExpr::Inf(true),
                 SymbolicExpr::Inf(false) => SymbolicExpr::Num(0),
+                SymbolicExpr::Mul(a, b) if matches!((a.as_ref(), b.as_ref()), (SymbolicExpr::I, SymbolicExpr::Pi) | (SymbolicExpr::Pi, SymbolicExpr::I)) => SymbolicExpr::Num(-1),
+                SymbolicExpr::Div(a, b) if matches!((b.as_ref()), (SymbolicExpr::Num(2))) && matches!(a.as_ref(), SymbolicExpr::Mul(x, y) if matches!((x.as_ref(), y.as_ref()), (SymbolicExpr::I, SymbolicExpr::Pi) | (SymbolicExpr::Pi, SymbolicExpr::I))) => SymbolicExpr::I,
+                SymbolicExpr::Add(a, b) => simplify_pass(SymbolicExpr::Mul(Box::new(SymbolicExpr::Exp(a)), Box::new(SymbolicExpr::Exp(b)))),
                 other => SymbolicExpr::Exp(Box::new(other)),
             }
         }
@@ -200,6 +217,10 @@ fn simplify_pass(expr: SymbolicExpr) -> SymbolicExpr {
                     SymbolicExpr::Ln(Box::new(SymbolicExpr::Div(x, y)))
                 }
 
+                (a, SymbolicExpr::Neg(b)) => {
+                    simplify_pass(SymbolicExpr::Add(Box::new(a), b))
+                }
+
                 (ref left, SymbolicExpr::Sub(ref sub_a, ref sub_b)) if left == sub_a.as_ref() => {
                     *sub_b.clone()
                 }
@@ -208,6 +229,9 @@ fn simplify_pass(expr: SymbolicExpr) -> SymbolicExpr {
                 (_, SymbolicExpr::Inf(false)) => SymbolicExpr::Inf(true),
                 (SymbolicExpr::Inf(true), _) => SymbolicExpr::Inf(true),
                 (SymbolicExpr::Inf(false), _) => SymbolicExpr::Inf(false),
+
+                (a, SymbolicExpr::Add(b, c)) if a == *b => SymbolicExpr::Neg(c),
+                (a, SymbolicExpr::Add(b, c)) if a == *c => SymbolicExpr::Neg(b),
 
                 (l, r) => SymbolicExpr::Sub(Box::new(l), Box::new(r)),
             }
@@ -255,6 +279,14 @@ fn simplify_pass(expr: SymbolicExpr) -> SymbolicExpr {
             match (a_red, b_red) {
                 (SymbolicExpr::Num(0), x) | (x, SymbolicExpr::Num(0)) => x,
                 (SymbolicExpr::Num(na), SymbolicExpr::Num(nb)) => SymbolicExpr::Num(na + nb),
+
+                (SymbolicExpr::Neg(x), y) => {
+                    simplify_pass(SymbolicExpr::Sub(Box::new(y), x))
+                }
+                (x, SymbolicExpr::Neg(y)) => {
+                    simplify_pass(SymbolicExpr::Sub(Box::new(x), y))
+                }
+
                 (l, r) => SymbolicExpr::Add(Box::new(l), Box::new(r)),
             }
         }
